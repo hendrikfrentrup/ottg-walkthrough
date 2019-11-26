71ac43f74e902788d3f88898fbd729b01da822e1
Hendrik Frentrup <hendrik.frentrup@gmail.com>
2019-11-26 11:19:29 +1100

    add FT for style testing

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -96,3 +96,27 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('buy a wetsuit', page_text)
         self.assertIn('buy notebook', page_text)
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # test for centered input box
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + 0.5*inputbox.size['width'],
+            512,
+            delta=10 
+        )
+
+         # type first item "buy surfboard" & hit enter, page updates
+        inputbox.send_keys("testing style")
+        inputbox.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table('1: testing style')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + 0.5*inputbox.size['width'],
+            512,
+            delta=10 
+        )
