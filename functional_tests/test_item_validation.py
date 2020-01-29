9aa253985d3eed17ddb371ae05e40a8c98abe9e5
Hendrik Frentrup <hendrik.frentrup@gmail.com>
2020-01-29 11:37:37 +1100

    test for item validation and generic wait helper func

--- functional_tests/test_item_validation.py
+++ functional_tests/test_item_validation.py
@@ -4,11 +4,32 @@ from unittest import skip
 
 class ItemValidationTest(FunctionalTest):
 
-    @skip("while writing test")
+    # @skip("while writing test")
     def test_cannot_add_empty_list_items(self):
-        self.fail('write me!')
         # visit homepage and submit empty list item
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
         # homepage refresh with error message
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
+
         # another submission with text, success
+        inputbox = self.browser.find_element_by_id('id_new_item').send_keys("buy drill")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: buy drill')
+
         # another empty submission, leading to error
-        # 
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
+        
+        # again another good submission with text, success
+        inputbox = self.browser.find_element_by_id('id_new_item').send_keys("buy screen")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: buy drill')
+        self.wait_for_row_in_list_table('2: buy screen')
