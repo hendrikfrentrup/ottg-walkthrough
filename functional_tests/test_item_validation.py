8d0bb59f81c95b6875e4bdbc2756858b76041179
Hendrik Frentrup <hendrik.frentrup@gmail.com>
2020-02-02 19:54:16 +1100

    test for validation error to disappear on input

--- functional_tests/test_item_validation.py
+++ functional_tests/test_item_validation.py
@@ -2,7 +2,11 @@ from .base import FunctionalTest
 from selenium.webdriver.common.keys import Keys
 from lists.forms import DUPLICATE_ITEM_ERROR
 
+
 class ItemValidationTest(FunctionalTest):
+
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
 
     def test_cannot_add_empty_list_items(self):
         # visit homepage and submit empty list item
@@ -41,6 +45,25 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             DUPLICATE_ITEM_ERROR
             ))
+
+    def test_error_messages_are_cleared_on_input(self):
+        # start list and cause validation error
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('some input')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: some input')
+        self.get_item_input_box().send_keys('some input')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # user inpy clears the error
+        self.get_item_input_box().send_keys('n')
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
+        ))
