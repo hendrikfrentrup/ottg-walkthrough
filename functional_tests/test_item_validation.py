2c5f575cb4f7691f27fb5db61617017cabd15c30
Hendrik Frentrup <hendrik.frentrup@gmail.com>
2020-02-03 11:50:18 +1100

    issue with validation FTs not running reliably, probably JS issue

--- functional_tests/test_item_validation.py
+++ functional_tests/test_item_validation.py
@@ -52,10 +52,10 @@ class ItemValidationTest(FunctionalTest):
     def test_error_messages_are_cleared_on_input(self):
         # start list and cause validation error
         self.browser.get(self.live_server_url)
-        self.get_item_input_box().send_keys('some input')
+        self.get_item_input_box().send_keys('keypress')
         self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: some input')
-        self.get_item_input_box().send_keys('some input')
+        self.wait_for_row_in_list_table('1: keypress')
+        self.get_item_input_box().send_keys('keypress')
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         self.wait_for(lambda: self.assertTrue(
@@ -71,10 +71,10 @@ class ItemValidationTest(FunctionalTest):
     def test_error_messages_are_cleared_on_click(self):
         # start list and cause validation error
         self.browser.get(self.live_server_url)
-        self.get_item_input_box().send_keys('some input')
+        self.get_item_input_box().send_keys('click')
         self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: some input')
-        self.get_item_input_box().send_keys('some input')
+        self.wait_for_row_in_list_table('1: click')
+        self.get_item_input_box().send_keys('click')
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         self.wait_for(lambda: self.assertTrue(
