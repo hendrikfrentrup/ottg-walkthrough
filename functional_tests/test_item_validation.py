d1ccbe319aea58214211f51dbfe8480412b5dd6a
Hendrik Frentrup <hendrik.frentrup@gmail.com>
2020-02-03 11:41:16 +1100

    make val errors disappear on click, with FT and unit tests

--- functional_tests/test_item_validation.py
+++ functional_tests/test_item_validation.py
@@ -67,3 +67,22 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for(lambda: self.assertFalse(
             self.get_error_element().is_displayed()
         ))
+
+    def test_error_messages_are_cleared_on_click(self):
+        # start list and cause validation error
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('some input')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: some input')
+        self.get_item_input_box().send_keys('some input')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # user selecting input box clears the error
+        self.get_item_input_box().click()
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
+        ))
--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,10 @@
-var initialize = function() {
-    $('input[name="text"').on('keypress', function(){
+window.Superlits = {};
+window.Superlits.initialize = function() {
+    var inputbox = $('input[name="text"')
+    inputbox.on('keypress', function(){
         $('.has-error').hide();
     });
-}
+    inputbox.on('click', function () {
+        $('.has-error').hide();
+    });
+};
