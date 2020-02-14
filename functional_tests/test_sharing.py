b9728cffede55ab6a30d9bb6bece3262f0fef6d0
Hendrik Frentrup <hendrik.frentrup@gmail.com>
2020-02-14 16:04:13 +1100

    create page objects for list pages and use in sharing FT

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from .base import FunctionalTest
+from .list_page import ListPage
+from .my_lists_page import MyListPage
 
 def quit_if_possible(browser):
     try: browser.quit()
@@ -22,12 +24,33 @@ class SharingTest(FunctionalTest):
         # user1 starts a list on the homepage
         self.browser = user1_browser
         self.browser.get(self.live_server_url)
-        self.add_list_item('user1 item')
+        list_page = ListPage(self).add_list_item('user1 item')
 
         # share list option is present on site
-        share_box = self.browser.find_element_by_css_selector(
-            'input[name="sharee"]'
-        )
+        share_box = list_page.get_share_box()
         self.assertEqual(
-            share_box.get_attribute('placeholder'), 'your-friends@email.com'
+            share_box.get_attribute('placeholder'), 
+            'your-friends@email.com'
         )
+
+        # user1 shares list with user2
+        list_page.share_list_with('also-exists@site-user.com')
+
+        # user2 goes to their lists page
+        self.browser = user2_browser
+        MyListPage(self).go_to_my_lists_page()
+
+        # user2 notices user1's list
+        self.browser.find_element_by_link_text('user1 item').click()
+
+        # user2 can see the list's owner is user 1
+        self.wait_for(lambda: self.assertEqual(
+            list_page.get_list_owner(),
+            'exists@site-user.com'
+        ))
+        # user 2 adds an item
+        list_page.add_list_item('hi user 1')
+        # user 1 notices added item upon refresh
+        self.browser = user1_browser
+        self.browser.refresh()
+        list_page.wait_for_row_in_list_table('hi user 1', 2)
